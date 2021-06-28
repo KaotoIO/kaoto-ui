0dba9b30d04b2b39e4781408e4fbed3da641a945
kahboom <rachelyordan@gmail.com>
2021-06-28 14:21:43 +0100

    add new monaco editor component

--- src/components/YAMLEditor.tsx
+++ src/components/YAMLEditor.tsx
@@ -1,12 +1,34 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import Editor from '@monaco-editor/react';
+import { useRef } from "react";
 
 const YAMLEditor = () => {
   React.useEffect(() => {
     console.log('Hello!');
   }, []);
 
-  return (<p>Edit your YAML file here.</p>);
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
+  function showValue() {
+    // @ts-ignore
+    alert(editorRef.current!.getValue());
+  }
+
+  return (
+    <>
+      <button onClick={showValue}>Show value</button>
+    <Editor
+      height="90vh"
+      defaultLanguage="YAML"
+      defaultValue="// edit your kamelet here"
+      onMount={handleEditorDidMount}
+    />
+      </>
+  );
 }
 
 export { YAMLEditor };
