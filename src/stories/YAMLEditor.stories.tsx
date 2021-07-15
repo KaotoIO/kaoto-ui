9ff280085b4ffbb0c745a47e92f8519c2849c2b1
kahboom <rachelyordan@gmail.com>
2021-07-15 11:12:11 +0100

    add backend dummy data for stories

--- src/stories/YAMLEditor.stories.tsx
+++ src/stories/YAMLEditor.stories.tsx
@@ -18,19 +18,3 @@ Primary.args = {
   label: 'Button',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
