52204bda15c381cb7577ead7be65e275b8e6d47c
kahboom <rachelyordan@gmail.com>
2021-07-23 15:48:47 +0100

    cleanup

--- src/components/YAMLEditor.tsx
+++ src/components/YAMLEditor.tsx
@@ -2,36 +2,14 @@ import * as React from 'react';
 import Editor from '@monaco-editor/react';
 import { useRef } from 'react';
 
-const YAMLEditor = ( props ) => {
-  React.useEffect(() => {
-    console.log('Hello!');
-  }, []);
-
+const YAMLEditor = ( initial ) => {
   const editorRef = useRef(null);
-  //const [YAML, setYAML] = React.useState('// edit your kamelet here');
-  //const [YAML, setYAML] = React.useState(JSON.stringify(response));
-  //const [YAML, setYAML] = React.useState(old);
+  const [YAML, setYAML] = React.useState(initial.yaml || '');
 
-  function handleEditorChange(value, event) {
-    // here is the current value
-    console.log("here is the current model value:", value);
-    //setYAML(value);
-  }
-
-  function handleEditorDidMount(editor, monaco) {
-    editorRef.current = editor;
-  }
-
-  function handleEditorValidation(markers) {
-    // model markers
-    markers.forEach(marker => console.log('onValidate:', marker.message));
-  }
-
-  function showValue() {
-    //alert(editorRef.current!.getValue());
-  }
-
-  // Example hook to retrieve data from an external endpoint
+  /**
+   * Hook to send YAML to external endpoint
+   * Returns JSON to be displayed in the visualizer
+   */
   function useFetchData() {
     const [status, setStatus] = React.useState('idle');
     const [data, setData] = React.useState({json: ''});
@@ -39,7 +17,7 @@ const YAMLEditor = ( props ) => {
     React.useEffect(() => {
       setStatus('loading');
       fetch('http://localhost:8080/viewdefinition?' + new URLSearchParams({
-        yaml: props.yaml
+        yaml: initial.yaml
       }), {
         method: 'POST',
         mode: 'no-cors',
@@ -72,9 +50,27 @@ const YAMLEditor = ( props ) => {
     };
   }
 
+  function handleEditorChange(value, event) {
+    console.log("Current model value:", value);
+    setYAML(value);
+  }
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
+  function handleEditorValidation(markers) {
+    // Model Markers
+    markers.forEach(marker => console.log('onValidate:', marker.message));
+  }
+
+  function showValue() {
+    //alert(editorRef.current!.getValue());
+  }
+
   const { data } = useFetchData();
 
-  const { json } = data;
+  console.log('Response to be used in visualizer: ' + JSON.stringify(data.json));
 
   return (
     <>
@@ -82,7 +78,7 @@ const YAMLEditor = ( props ) => {
       <Editor
         height="90vh"
         defaultLanguage="JSON"
-        defaultValue={json}
+        defaultValue={YAML || initial.yaml}
         onChange={handleEditorChange}
         onMount={handleEditorDidMount}
         onValidate={handleEditorValidation}
