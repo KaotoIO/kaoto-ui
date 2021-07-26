a974b6b59ec1df795574b81ab7e79ab4fca4a19d
kahboom <rachelyordan@gmail.com>
2021-07-26 16:05:14 +0100

    cleanup

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,9 @@
-import { PageSection, Title } from '@patternfly/react-core';
+import { PageSection } from '@patternfly/react-core';
 import { YAMLEditor } from '../components/YAMLEditor';
 
 const Dashboard = () => {
   return (
     <PageSection>
-      <Title headingLevel="h1" size="lg">
-        Zimara
-      </Title>
-      <p>Testing something..</p>
       <YAMLEditor />
     </PageSection>
   );
