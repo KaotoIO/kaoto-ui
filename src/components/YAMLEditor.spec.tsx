df4e38f19b7704e474066eb39aac45d440b03e04
kahboom <rachelyordan@gmail.com>
2021-07-30 10:26:32 +0100

    set up test for YAMLEditor

---
+++ src/components/YAMLEditor.spec.tsx
@@ -0,0 +1,17 @@
+//import * as React from 'react';
+import { render } from '@testing-library/react';
+import { YAMLEditor } from './YAMLEditor';
+
+const handleChangesMock = jest.fn();
+
+afterEach(() => {
+  handleChangesMock.mockReset();
+});
+
+const renderMocked = () => {
+  return render(
+    <YAMLEditor handleChanges={handleChangesMock}/>
+  );
+};
+
+it('', () => {});
