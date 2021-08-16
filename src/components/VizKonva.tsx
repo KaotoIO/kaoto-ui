9d26e036e11115e742b93484c21ee853861a9935
kahboom <rachelyordan@gmail.com>
2021-08-16 17:10:18 +0100

    add basic nodes and line for konva

--- src/components/VizKonva.tsx
+++ src/components/VizKonva.tsx
@@ -1,28 +1,95 @@
 import * as React from 'react';
-import { Stage, Layer, Rect, Text } from 'react-konva';
-import Konva from 'konva';
+import { Circle, Group, Layer, Line, Stage } from 'react-konva';
+import { v4 as uuidv4 } from 'uuid';
+//import { IStepProps } from '../types';
 
-const ColoredRect = () => {
-  const [color, setColor] = React.useState('green');
+interface IVizKonva {
+  isError?: boolean;
+  isLoading?: boolean;
+  steps?: any[];
+}
 
-  const handleClick = () => {
-    setColor(Konva.Util.getRandomColor())
-  };
-
+const ConnectingLine = () => {
   return (
-    <Rect
-      x={20}
-      y={20}
-      width={50}
-      height={50}
-      fill={color}
-      shadowBlur={5}
-      onClick={handleClick}
+    <Line
+      points={
+        // points format: [x1, y1, x2, y2, x3, y3]
+        [
+          0, window.innerHeight / 2,
+          window.innerWidth, window.innerHeight / 2
+        ]
+      }
+      stroke={'black'}
+      strokeWidth={2}
+      lineCap={'round'}
+      lineJoin={'round'}
     />
   );
 };
 
-const VizKonva = () => {
+const CIRCLE_LENGTH = 75;
+
+const VizKonva = ({ isError, isLoading, steps }: IVizKonva) => {
+  const [group, setGroup] = React.useState({ x: 0, y: 0 });
+  const yAxis = window.innerHeight / 2;
+  const [nodes, setNodes] = React.useState([
+    { x: 100, y: yAxis },
+    { x: 200, y: yAxis },
+    { x: 300, y: yAxis },
+    { x: 400, y: yAxis }
+  ]);
+
+  const stepsAsElements: any[] = [];
+
+  steps.map((step, index) => {
+    const currentStepId = uuidv4();
+
+    let inputStep = {
+      data: { label: step.name },
+      id: currentStepId,
+      position: { x: 300, y: 200 },
+      type: undefined
+    };
+
+    // Grab the previous step to use for determining position and drawing edges
+    const previousStep = stepsAsElements[index-1];
+
+    /**
+     * Determine first & last steps
+     * Label as input/output, respectively
+     */
+    switch(index) {
+      case 0:
+        // First item in `steps` array
+        //inputStep.type = 'input';
+        break;
+      case steps.length - 1:
+        // Last item in `steps` array
+        //inputStep.type = 'output';
+        // Extract into common area for last & middle steps
+        //inputStep.position!.y = previousStep.position?.y + 100;
+
+        // Add edges
+        //stepsAsElements.push({id: 'EDGE-' + previousStep.id + '--' + currentStepId, source: previousStep.id,
+        // target: currentStepId, animated: true});
+        break;
+      default:
+        // Middle step
+        //inputStep.position!.y = previousStep.position!.y + 100;
+
+        // Add edges
+        //stepsAsElements.push({id: 'EDGE-' + previousStep.id + '--' + currentStepId, source: previousStep.id,
+        // target: currentStepId, animated: true});
+        break;
+    }
+
+    stepsAsElements.push(inputStep);
+
+    return;
+  });
+
+  const onDragEnd = e => {};
+
   // Stage is a div container
   // Layer is actual canvas element (so you may have several canvases in the stage)
   // And then we have canvas shapes inside the Layer
@@ -31,8 +98,24 @@ const VizKonva = () => {
       <h1>Konva</h1>
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
-          <Text text="Click the rectangle" />
-          <ColoredRect />
+          <Group x={group.x} y={group.y} onDragEnd={onDragEnd} draggable>
+            <ConnectingLine/>
+            {nodes.map((item, index) => {
+              return (
+                <Circle
+                  x={item.x}
+                  y={item.y}
+                  key={index}
+                  name={`${index}`}
+                  stroke={'grey'}
+                  fill={'white'}
+                  strokeWidth={2}
+                  width={CIRCLE_LENGTH}
+                  height={CIRCLE_LENGTH}
+                />
+              )
+            })}
+          </Group>
         </Layer>
       </Stage>
     </>
