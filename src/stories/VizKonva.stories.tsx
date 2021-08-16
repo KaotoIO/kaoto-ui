62fe11d54776d629592bab92c0359474f7f2ef72
kahboom <rachelyordan@gmail.com>
2021-08-16 17:21:29 +0100

    extract dummy steps into separate file, update story

--- src/stories/VizKonva.stories.tsx
+++ src/stories/VizKonva.stories.tsx
@@ -3,13 +3,24 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { VizKonva } from '../components/VizKonva';
 
+import steps from './steps';
+
 export default {
   title: 'Visualization/Konva',
   component: VizKonva,
 } as ComponentMeta<typeof VizKonva>;
 
 //const Template: ComponentStory<typeof VizKonva> = (args) => <VizKonva {...args} />;
-const Template: ComponentStory<typeof VizKonva> = (args) => <VizKonva />;
+const Template: ComponentStory<typeof VizKonva> = (args) => {
+  // For now the only view data we care about are steps
+  const viewData = {
+    steps
+  };
+
+  return (
+    <VizKonva steps={viewData.steps} />
+  );
+};
 
 export const Primary = Template.bind({});
 //Primary.args = {};
