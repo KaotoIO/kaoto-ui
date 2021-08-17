47184d6d4816a6da5f33c38ba4c49cdcba092009
kahboom <rachelyordan@gmail.com>
2021-08-17 13:19:13 +0100

    rm hook, calculate first and last step positions

--- src/components/VizKonva.tsx
+++ src/components/VizKonva.tsx
@@ -1,45 +1,29 @@
 import * as React from 'react';
 import { Circle, Group, Layer, Line, Stage } from 'react-konva';
 import { v4 as uuidv4 } from 'uuid';
-//import { IStepProps } from '../types';
+import { IStepProps } from '../types';
 
 interface IVizKonva {
   isError?: boolean;
   isLoading?: boolean;
-  steps?: any[];
+  steps: IStepProps[];
 }
-
-const ConnectingLine = () => {
-  return (
-    <Line
-      points={
-        // points format: [x1, y1, x2, y2, x3, y3]
-        [
-          0, window.innerHeight / 2,
-          window.innerWidth, window.innerHeight / 2
-        ]
-      }
-      stroke={'black'}
-      strokeWidth={2}
-      lineCap={'round'}
-      lineJoin={'round'}
-    />
-  );
-};
 
 const CIRCLE_LENGTH = 75;
 
 const VizKonva = ({ isError, isLoading, steps }: IVizKonva) => {
-  const [group, setGroup] = React.useState({ x: 0, y: 0 });
+  const groupPosition = { x: 0, y: 0 };
   const yAxis = window.innerHeight / 2;
-  const [nodes, setNodes] = React.useState([
-    { x: 100, y: yAxis },
-    { x: 200, y: yAxis },
-    { x: 300, y: yAxis },
-    { x: 400, y: yAxis }
-  ]);
+
+  const incrementAmt = 100;
 
   const stepsAsElements: any[] = [];
+
+  // points format: [x1, y1, x2, y2, x3, y3]
+  const linePosition = [
+    100, window.innerHeight / 2,
+    steps.length * incrementAmt, window.innerHeight / 2
+  ];
 
   steps.map((step, index) => {
     const currentStepId = uuidv4();
@@ -47,12 +31,13 @@ const VizKonva = ({ isError, isLoading, steps }: IVizKonva) => {
     let inputStep = {
       data: { label: step.name },
       id: currentStepId,
-      position: { x: 300, y: 200 },
+      position: { x: 300, y: yAxis },
       type: undefined
     };
 
     // Grab the previous step to use for determining position and drawing edges
     const previousStep = stepsAsElements[index-1];
+
 
     /**
      * Determine first & last steps
@@ -61,13 +46,12 @@ const VizKonva = ({ isError, isLoading, steps }: IVizKonva) => {
     switch(index) {
       case 0:
         // First item in `steps` array
-        //inputStep.type = 'input';
+        inputStep.position.x = 100;
         break;
       case steps.length - 1:
         // Last item in `steps` array
-        //inputStep.type = 'output';
         // Extract into common area for last & middle steps
-        //inputStep.position!.y = previousStep.position?.y + 100;
+        inputStep.position.x = previousStep.position?.x + incrementAmt;
 
         // Add edges
         //stepsAsElements.push({id: 'EDGE-' + previousStep.id + '--' + currentStepId, source: previousStep.id,
@@ -75,7 +59,7 @@ const VizKonva = ({ isError, isLoading, steps }: IVizKonva) => {
         break;
       default:
         // Middle step
-        //inputStep.position!.y = previousStep.position!.y + 100;
+        inputStep.position.x = previousStep.position!.x + incrementAmt;
 
         // Add edges
         //stepsAsElements.push({id: 'EDGE-' + previousStep.id + '--' + currentStepId, source: previousStep.id,
@@ -98,13 +82,19 @@ const VizKonva = ({ isError, isLoading, steps }: IVizKonva) => {
       <h1>Konva</h1>
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
-          <Group x={group.x} y={group.y} onDragEnd={onDragEnd} draggable>
-            <ConnectingLine/>
-            {nodes.map((item, index) => {
+          <Group x={groupPosition.x} y={groupPosition.y} onDragEnd={onDragEnd} draggable>
+            <Line
+              points={linePosition}
+              stroke={'black'}
+              strokeWidth={2}
+              lineCap={'round'}
+              lineJoin={'round'}
+            />
+            {stepsAsElements.map((item, index) => {
               return (
                 <Circle
-                  x={item.x}
-                  y={item.y}
+                  x={item.position.x}
+                  y={item.position.y}
                   key={index}
                   name={`${index}`}
                   stroke={'grey'}
