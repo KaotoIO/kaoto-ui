d05ffed375e4c426443bdf835ad51bbb99cdf536
kahboom <rachelyordan@gmail.com>
2021-08-17 18:33:33 +0100

    reposition group, add image with corresponding props

--- src/components/VizKonva.tsx
+++ src/components/VizKonva.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import { Circle, Group, Layer, Line, Stage } from 'react-konva';
+import { Circle, Group, Image, Layer, Line, Stage } from 'react-konva';
 import { v4 as uuidv4 } from 'uuid';
 import { IStepProps } from '../types';
 
@@ -12,32 +11,24 @@ interface IVizKonva {
 const CIRCLE_LENGTH = 75;
 
 const VizKonva = ({ isError, isLoading, steps }: IVizKonva) => {
-  const groupPosition = { x: 0, y: 0 };
   const yAxis = window.innerHeight / 2;
 
   const incrementAmt = 100;
 
   const stepsAsElements: any[] = [];
 
-  // points format: [x1, y1, x2, y2, x3, y3]
-  const linePosition = [
-    100, window.innerHeight / 2,
-    steps.length * incrementAmt, window.innerHeight / 2
-  ];
-
   steps.map((step, index) => {
     const currentStepId = uuidv4();
 
     let inputStep = {
+      ...step,
       data: { label: step.name },
       id: currentStepId,
-      position: { x: 300, y: yAxis },
-      type: undefined
+      position: { x: 300, y: yAxis }
     };
 
     // Grab the previous step to use for determining position and drawing edges
     const previousStep = stepsAsElements[index-1];
-
 
     /**
      * Determine first & last steps
@@ -49,21 +40,10 @@ const VizKonva = ({ isError, isLoading, steps }: IVizKonva) => {
         inputStep.position.x = 100;
         break;
       case steps.length - 1:
-        // Last item in `steps` array
+      default:
+        // Last item & middle steps in `steps` array
         // Extract into common area for last & middle steps
         inputStep.position.x = previousStep.position?.x + incrementAmt;
-
-        // Add edges
-        //stepsAsElements.push({id: 'EDGE-' + previousStep.id + '--' + currentStepId, source: previousStep.id,
-        // target: currentStepId, animated: true});
-        break;
-      default:
-        // Middle step
-        inputStep.position.x = previousStep.position!.x + incrementAmt;
-
-        // Add edges
-        //stepsAsElements.push({id: 'EDGE-' + previousStep.id + '--' + currentStepId, source: previousStep.id,
-        // target: currentStepId, animated: true});
         break;
     }
 
@@ -72,7 +52,19 @@ const VizKonva = ({ isError, isLoading, steps }: IVizKonva) => {
     return;
   });
 
-  const onDragEnd = e => {};
+  const createImage = (url, crossOrigin?) => {
+    if (!url) return;
+    const img = document.createElement('img');
+    img.crossOrigin = crossOrigin;
+    img.src = url;
+
+    return img;
+  };
+
+  const imageProps = {
+    height: 40,
+    width: 40
+  };
 
   // Stage is a div container
   // Layer is actual canvas element (so you may have several canvases in the stage)
@@ -82,27 +74,41 @@ const VizKonva = ({ isError, isLoading, steps }: IVizKonva) => {
       <h1>Konva</h1>
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
-          <Group x={groupPosition.x} y={groupPosition.y} onDragEnd={onDragEnd} draggable>
+          <Group x={100} y={200} draggable>
             <Line
-              points={linePosition}
+              points={[
+                100, 0,
+                steps.length * incrementAmt, 0
+              ]}
               stroke={'black'}
               strokeWidth={2}
               lineCap={'round'}
               lineJoin={'round'}
             />
             {stepsAsElements.map((item, index) => {
+              const image = {
+                image: createImage(item.icon),
+                x: item.position.x - (imageProps.width / 2),
+                y: 0 - (imageProps.height / 2),
+                height: imageProps.height,
+                width: imageProps.width
+              };
+
               return (
-                <Circle
-                  x={item.position.x}
-                  y={item.position.y}
-                  key={index}
-                  name={`${index}`}
-                  stroke={'grey'}
-                  fill={'white'}
-                  strokeWidth={2}
-                  width={CIRCLE_LENGTH}
-                  height={CIRCLE_LENGTH}
+                <Group key={index}>
+                  <Circle
+                    x={item.position.x}
+                    y={0}
+                    key={index}
+                    name={`${index}`}
+                    stroke={'grey'}
+                    fill={'white'}
+                    strokeWidth={2}
+                    width={CIRCLE_LENGTH}
+                    height={CIRCLE_LENGTH}
                 />
+                  <Image {...image} />
+                </Group>
               )
             })}
           </Group>
