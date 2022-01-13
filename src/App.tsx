931c21f03d0b8276ee73dbdfd644130d4a9909e2
kahboom <rachelyordan@gmail.com>
2022-01-13 13:49:39 +0000

    fix for patternfly imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import './App.css';
 import { AlertProvider, MASLoading } from './components';
 import { AppLayout } from './layouts/AppLayout';
 import { AppRoutes } from './routes';
-import '@patternfly/react-core/dist/styles/base.css';
-import { FunctionComponent, Suspense } from 'react';
+import '@patternfly/patternfly/patternfly.css';
+import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
+import '@patternfly/patternfly/utilities/Display/display.css';
+import '@patternfly/patternfly/utilities/Flex/flex.css';
+import '@patternfly/patternfly/utilities/Sizing/sizing.css';
+import '@patternfly/patternfly/utilities/Spacing/spacing.css';
+import { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-export const App: FunctionComponent = () => {
+export const App = () => {
   return (
     <AlertProvider>
       <Router>
