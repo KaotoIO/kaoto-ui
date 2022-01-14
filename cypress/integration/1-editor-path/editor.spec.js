70a3cabcd475f7cc76f684bacd3338c938792cd6
kahboom <rachelyordan@gmail.com>
2022-01-14 15:34:06 +0000

    test verifying yaml editor loads as expected

---
+++ cypress/integration/1-editor-path/editor.spec.js
@@ -0,0 +1,9 @@
+describe('editor flow', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:1337');
+  });
+
+  it('loads the YAML editor', () => {
+    cy.get('.code-editor').should('exist');
+  });
+});
