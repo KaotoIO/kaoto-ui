f7e5f6c85abbda68f0a44c047dbef245bb8c7295
kahboom <rachelyordan@gmail.com>
2022-02-10 13:22:50 +0000

    upgrade storybook, update mock data for stories

--- src/stories/data/views.ts
+++ src/stories/data/views.ts
@@ -1,41 +1,59 @@
 export default [
   {
-    "id": "detail-step",
-    "name": "Detail View",
-    "type": "step"
+    id: 'detail-step',
+    name: 'Detail View',
+    type: 'step',
   },
   {
-    "id": "detail-step",
-    "name": "Detail View",
-    "type": "step"
+    id: 'detail-step',
+    name: 'Detail View',
+    type: 'step',
   },
   {
-    "id": "kameletBinding",
-    "name": "Kamelet Binding",
-    "type": "generic",
-    "constraints": [
+    id: 'custom-step',
+    name: 'Kafka Properties',
+    type: 'step',
+    module: './Button',
+    scope: 'stepextension',
+    step: '3kafka-sink',
+    url: 'https://step-extension.netlify.app/remoteEntry.js',
+  },
+  {
+    id: 'detail-step',
+    name: 'Custom Details',
+    type: 'step',
+    module: './Button',
+    scope: 'stepextension',
+    step: '0twitter-search-source',
+    url: 'https://step-extension.netlify.app/remoteEntry.js',
+  },
+  {
+    id: 'kameletBinding',
+    name: 'Kamelet Binding',
+    type: 'generic',
+    constraints: [
       {
-        "mandatory": true,
-        "operation": "SIZE_GREATER_THAN",
-        "parameter": "1"
+        mandatory: true,
+        operation: 'SIZE_GREATER_THAN',
+        parameter: '1',
       },
       {
-        "mandatory": true,
-        "operation": "CONTAINS_STEP_TYPE",
-        "parameter": "KAMELET"
-      }
-    ]
+        mandatory: true,
+        operation: 'CONTAINS_STEP_TYPE',
+        parameter: 'KAMELET',
+      },
+    ],
   },
   {
-    "id": "integration",
-    "name": "Integration View",
-    "type": "generic",
-    "constraints": [
+    id: 'integration',
+    name: 'Integration View',
+    type: 'generic',
+    constraints: [
       {
-        "mandatory": true,
-        "operation": "SIZE_GREATER_THAN",
-        "parameter": "1"
-      }
-    ]
-  }
+        mandatory: true,
+        operation: 'SIZE_GREATER_THAN',
+        parameter: '1',
+      },
+    ],
+  },
 ];
