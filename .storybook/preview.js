63cd171e5560a8597ba4ed59fd2754ec30da32e4
kahboom <rachelyordan@gmail.com>
2022-02-16 17:32:44 +0000

    performance improvement storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,18 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  features: {
+    storyStoreV7: true,
+  },
+  stories: [
+    {
+      directory: '../src',
+      files: '*.stories.*',
+    },
+  ],
+};
