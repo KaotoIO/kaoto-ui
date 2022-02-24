989c4014757a92fadf9608b904c4578d881434dc
kahboom <rachelyordan@gmail.com>
2022-02-24 16:24:42 +0000

    minor fix for mini catalog story

--- src/components/MiniCatalog.stories.tsx
+++ src/components/MiniCatalog.stories.tsx
@@ -1,5 +1,6 @@
 import catalog from '../data/catalog';
 import { MiniCatalog, IMiniCatalog } from './MiniCatalog';
+import { action } from '@storybook/addon-actions';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 export default {
@@ -17,5 +18,6 @@ const Template: ComponentStory<typeof MiniCatalog> = (props: IMiniCatalog) => {
 
 export const Primary = Template.bind({});
 Primary.args = {
+  handleSelectStep: action('Step selected'),
   steps: catalog,
 };
