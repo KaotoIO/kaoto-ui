ec06c7bdc45c53203bf502f411b948f5c82bfb92
kahboom <rachelyordan@gmail.com>
2022-03-07 15:18:19 +0000

    disable ability to connect steps until temporary steps are reimplemented

--- src/components/VisualizationStep.tsx
+++ src/components/VisualizationStep.tsx
@@ -28,7 +28,13 @@ const VisualizationStep = ({ data }: any) => {
         onDrop={onDropChange}
       >
         {data.connectorType !== 'END' && !isLastNode && (
-          <Handle type="source" position={Position.Right} id="b" style={{ borderRadius: 0 }} />
+          <Handle
+            isConnectable={false}
+            type="source"
+            position={Position.Right}
+            id="b"
+            style={{ borderRadius: 0 }}
+          />
         )}
         {data.connectorType !== 'END' && isLastNode && (
           <Popover
@@ -49,7 +55,13 @@ const VisualizationStep = ({ data }: any) => {
           <img src={data.icon} className="nodrag" alt={data.label} />
         </div>
         {data.connectorType !== 'START' && (
-          <Handle type="target" position={Position.Left} id="a" style={{ borderRadius: 0 }} />
+          <Handle
+            isConnectable={false}
+            type="target"
+            position={Position.Left}
+            id="a"
+            style={{ borderRadius: 0 }}
+          />
         )}
         <div className={'stepNode__Label'} onClick={onElementClick}>
           {data.label}
