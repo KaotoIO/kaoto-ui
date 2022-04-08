24ff50e771369946d2f9d00ec5c0bdc5bb536589
kahboom <rachelyordan@gmail.com>
2022-04-08 15:11:32 +0100

    cleanup

--- src/api/request.ts
+++ src/api/request.ts
@@ -32,12 +32,11 @@ export interface IFetch {
 }
 declare var window: any;
 let apiURL = process.env.KAOTO_API;
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV == 'production' && window.KAOTO_API) {
   apiURL = window.KAOTO_API;
 }
-
 
 // converts an object into a query string
 // ex: {authorId : 'abc123'} -> &type=Kamelet
