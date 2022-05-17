f097069c9979b2a1c079d8bdfb0718c2ac83d05c
Mohammadi Iram <mohammadiiram@gmail.com>
2022-05-17 15:54:06 +0530

    Add cypress code for editing

--- cypress/integration/editing/editing.spec.js
+++ cypress/integration/editing/editing.spec.js
@@ -7,23 +7,26 @@ describe('editing properties', () => {
     cy.get('.code-editor').click();
     cy.get('button').contains('Start from scratch').click();
      
-    cy.fixture('editor.txt').then((user) => {
-      cy.get('.code-editor').type(user)
+    cy.fixture('editor.txt')
+    .then((user) => {
+      cy.get('.code-editor').type(user);
+    })
+    .then(() => {
       cy.get('[data-id="dndnode_2"]').click();
       cy.get('[data-testid="configurationTab"]').click();
       cy.get('[data-testid="json-schema-configurator"]').click();
       cy.get('input[name="topic"]').type('Hello');
-      cy.get('.code-editor').should('contain.text','Hello');
+      cy.get('.code-editor').should('contain.text', 'Hello');
       cy.get('input[name="bootstrapServers"]').type('anything');
-      cy.get('.code-editor').should('contain.text','anything');
+      cy.get('.code-editor').should('contain.text', 'anything');
       cy.get('input[name="securityProtocol"]').type('anything');
-      cy.get('.code-editor').should('contain.text','anything');
+      cy.get('.code-editor').should('contain.text', 'anything');
       cy.get('input[name="saslMechanism"]').type('anything');
-      cy.get('.code-editor').should('contain.text','anything');
-      cy.get('input[name="user"]').type('anything');  
-      cy.get('.code-editor').should('contain.text','anything'); 
+      cy.get('.code-editor').should('contain.text', 'anything');
+      cy.get('input[name="user"]').type('anything');
+      cy.get('.code-editor').should('contain.text', 'anything');
       cy.get('input[name="password"]').type('abc{enter}');
-      cy.get('.code-editor').should('contain.text','abc');
-    }) ;
+      cy.get('.code-editor').should('contain.text', 'abc');
+    });
   });
 });
