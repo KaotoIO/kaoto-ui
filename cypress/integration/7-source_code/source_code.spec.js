db843427e2d3a19f2fb58bfb07ef731320103bac
Mohammadi Iram <mohammadiiram@gmail.com>
2022-05-27 13:31:05 +0530

    Add cypress test that combines source code and drag and drop

---
+++ cypress/integration/7-source_code/source_code.spec.js
@@ -0,0 +1,27 @@
+describe('source code and drag and drop', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:1337');
+    });
+    it('loads the YAML editor', () => {
+        const dataTransfer = new DataTransfer();
+        cy.get('.code-editor').click();
+        cy.get('button').contains('Start from scratch').click();
+        cy.fixture('editor.txt')
+            .then((user) => {
+                cy.get('.code-editor').type(user);
+                cy.get('[data-testid="openCatalogButton"] > svg').click();
+                cy.get('#stepSearch').type('timer')
+                cy.get('.pf-c-card__body').trigger('dragstart', {
+                    dataTransfer
+                });
+                cy.get('[data-id="dndnode_1"]').trigger('drop', {
+                    dataTransfer
+                });
+                cy.get('.pf-c-drawer__close > .pf-c-button').click();
+                cy.get('.code-editor').click();
+                cy.get('[style="top:323px;height:19px;"] > :nth-child(1) > .mtk5').click();
+                // cy.get('[style="top:323px;height:19px;"] > :nth-child(1) > .mtk5').clear({force: true})
+                // cy.contains('timer-source').type('kafka');
+            })
+    })
+})
