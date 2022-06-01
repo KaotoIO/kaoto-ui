ed6ad6c19077b3fc0ab2da8ff711143b5347b300
Mohammadi Iram <mohammadiiram@gmail.com>
2022-06-01 17:36:24 +0530

    Changed the selector

--- cypress/integration/8-deleting_steps/deleting_steps.spec.js
+++ cypress/integration/8-deleting_steps/deleting_steps.spec.js
@@ -9,13 +9,13 @@ describe('Test for deleting steps', () => {
             .then((user) => {
                 cy.get('.code-editor').type(user);
                 cy.wait(1000)
-                cy.get('.stepNode').contains('chuck-norris-s..').click()
+                cy.get('[data-testid="react-flow-wrapper"]').contains('chuck-norris').click();
                 cy.wait(1000)
                 cy.get('[data-ouia-component-id="OUIA-Generated-Button-danger-1"]').click()
                 cy.get('.code-editor').contains('chuck-norris-s..').should('not.exist');
-                cy.get('.stepNode').contains('chunk-template..').click()
+                cy.get('[data-testid="react-flow-wrapper"]').contains('chunk-template..').click()
                 cy.get('[data-ouia-component-id="OUIA-Generated-Button-danger-5"]').click()
-                cy.get('.stepNode').contains('kafka-sink').click()
+                cy.get('[data-testid="react-flow-wrapper"]').contains('kafka-sink').click()
                 cy.get('[data-ouia-component-id="OUIA-Generated-Button-danger-9"]').click()
             });
     })
