0de9781471f407ec41f300dd2b5293b2e84cda1b
Mohammadi Iram <mohammadiiram@gmail.com>
2022-06-09 16:58:51 +0530

    Cypress test for Undo/Redo actions in the code editor

---
+++ cypress/integration/9-undo_redo_actions/undo_redo_actions.spec.js
@@ -0,0 +1,23 @@
+describe('Test for undo/redo actions on code-editor', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:1337');
+    });
+    it('loads the YAML editor', () => {
+        const dataTransfer = new DataTransfer();
+        cy.get('.code-editor').click();
+        cy.get('button').contains('Start from scratch').click();
+        cy.fixture('undo_redo.txt')
+            .then((user) => {
+                cy.get('.code-editor').type(user);
+                cy.wait(2000)
+                cy.get('.code-editor')
+                    .contains('kafka-source')
+                    .should('have.text', 'kafka-source', '{backspace}')
+                    .type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}timer-', { delay: 500 });
+                    cy.get('[aria-label="Undo change"] > svg').click()
+                    cy.get('[aria-label="Undo change"] > svg').click()
+                    cy.get('[aria-label="Redo change"] > svg').click()
+                    cy.get('[aria-label="Redo change"] > svg').click()
+                })           
+            })
+    })
