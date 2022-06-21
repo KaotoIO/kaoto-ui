9b2480f0d5bb26eabc8baa97618d0ef9150d78d9
Mohammadi Iram <mohammadiiram@gmail.com>
2022-06-21 14:14:00 +0530

    Cypress test to Make Kaoto location configurable

--- cypress/integration/10-configurable/configurable.spec.js
+++ cypress/integration/10-configurable/configurable.spec.js
@@ -1,6 +1,6 @@
 describe('Kaoto Location configurable', () => {
-    it('Kaoto Location configurable', () => {
-        let url = Cypress.config().baseUrl; 
-        cy.visit(url); 
-    });
+  it('Kaoto Location configurable', () => {
+    let url = Cypress.config().baseUrl;
+    cy.visit(url);
+  });
 });
