0abc1e30b60918cd2ef1cfa14a4a202adc283fbe
Mohammadi Iram <mohammadiiram@gmail.com>
2022-06-23 12:16:25 +0530

    Changed to configured url

--- cypress/integration/5-editing/editing.spec.js
+++ cypress/integration/5-editing/editing.spec.js
@@ -1,6 +1,7 @@
 describe('editing properties', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:1337');
+    let url = Cypress.config().baseUrl;
+        cy.visit(url);
   });
 
   it('loads the YAML editor', () => {
