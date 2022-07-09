7f2bd4aca825361220b792b7fb2f17e2bc47cbdf
kahboom <rachelyordan@gmail.com>
2022-07-09 16:02:57 +0100

    add sticky banner to footer, hide screen reader text by default

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -9,7 +9,7 @@ import {
 import { KaotoToolbar } from '../components/KaotoToolbar';
 import { IViewProps } from '../types';
 import './Dashboard.css';
-import { Page, PageSection, GridItem, Grid } from '@patternfly/react-core';
+import { Page, PageSection, GridItem, Grid, Flex, FlexItem, Banner } from '@patternfly/react-core';
 import { useState } from 'react';
 
 export interface IExpanded {
@@ -42,49 +42,79 @@ const Dashboard = () => {
     <IntegrationJsonProvider initialState={{ metadata: { name: '' }, params: [], steps: [] }}>
       <IntegrationSourceProvider initialState={''}>
         <SettingsProvider>
-          <Page>
-            <PageSection padding={{ default: 'noPadding' }}>
-              <KaotoToolbar
-                deployment={deployment}
-                expanded={expanded}
-                handleExpanded={handleExpanded}
-                handleSaveDeployment={handleSaveDeployment}
-              />
-              <Grid>
-                {expanded.codeEditor ? (
-                  <GridItem span={3}>
-                    <SourceCodeEditor
-                      handleUpdateViews={(newViews: IViewProps[]) => {
-                        if (newViews === views) return;
-                        setViews(newViews);
-                      }}
-                    />
-                  </GridItem>
-                ) : expanded.catalog ? (
-                  <GridItem span={3}>
-                    <Catalog currentDeployment={deployment} />
-                  </GridItem>
-                ) : (
-                  <></>
-                )}
-                <GridItem
-                  span={expanded.codeEditor || expanded.catalog ? 9 : 12}
-                  className={'visualization'}
-                >
-                  <Visualization
-                    handleUpdateViews={(newViews: IViewProps[]) => {
-                      if (newViews === views) return;
-                      setViews(newViews);
-                    }}
-                    toggleCatalog={() =>
-                      setExpanded({ ...expanded, catalog: !expanded.catalog, codeEditor: false })
-                    }
-                    views={views}
+          <Flex
+            direction={{ default: 'column' }}
+            flexWrap={{ default: 'nowrap' }}
+            spaceItems={{ default: 'spaceItemsNone' }}
+            style={{ height: '100%' }}
+          >
+            <FlexItem>
+              <Page>
+                <PageSection padding={{ default: 'noPadding' }}>
+                  <KaotoToolbar
+                    deployment={deployment}
+                    expanded={expanded}
+                    handleExpanded={handleExpanded}
+                    handleSaveDeployment={handleSaveDeployment}
                   />
-                </GridItem>
-              </Grid>
-            </PageSection>
-          </Page>
+                  <Grid>
+                    {expanded.codeEditor ? (
+                      <GridItem span={3}>
+                        <SourceCodeEditor
+                          handleUpdateViews={(newViews: IViewProps[]) => {
+                            if (newViews === views) return;
+                            setViews(newViews);
+                          }}
+                        />
+                      </GridItem>
+                    ) : expanded.catalog ? (
+                      <GridItem span={3}>
+                        <Catalog currentDeployment={deployment} />
+                      </GridItem>
+                    ) : (
+                      <></>
+                    )}
+                    <GridItem
+                      span={expanded.codeEditor || expanded.catalog ? 9 : 12}
+                      className={'visualization'}
+                    >
+                      <Visualization
+                        handleUpdateViews={(newViews: IViewProps[]) => {
+                          if (newViews === views) return;
+                          setViews(newViews);
+                        }}
+                        toggleCatalog={() =>
+                          setExpanded({
+                            ...expanded,
+                            catalog: !expanded.catalog,
+                            codeEditor: false,
+                          })
+                        }
+                        views={views}
+                      />
+                    </GridItem>
+                  </Grid>
+                </PageSection>
+                {/*<PageSection sticky={'bottom'}>Hello</PageSection>*/}
+              </Page>
+            </FlexItem>
+            <FlexItem>
+              <Banner isSticky={true} screenReaderText="Status">
+                <Flex
+                  justifyContent={{
+                    default: 'justifyContentCenter',
+                    lg: 'justifyContentSpaceBetween',
+                  }}
+                  flexWrap={{ default: 'nowrap' }}
+                >
+                  <div>Localhost</div>
+                  <div>This message is sticky to the bottom of the page.</div>
+                  <div>Drop some text on mobile, truncate if needed.</div>
+                  <div>Ned Username</div>
+                </Flex>
+              </Banner>
+            </FlexItem>
+          </Flex>
           <DeploymentsModal
             currentDeployment={deployment}
             handleCloseModal={() => {
