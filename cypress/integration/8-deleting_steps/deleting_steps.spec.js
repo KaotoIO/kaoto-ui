418e5bbce86c979bbcb8b1b28344c87106c97806
Mohammadi Iram <mohammadiiram@gmail.com>
2022-08-04 15:20:53 +0530

    Updating Breaking E2E Tests

--- cypress/integration/8-deleting_steps/deleting_steps.spec.js
+++ cypress/integration/8-deleting_steps/deleting_steps.spec.js
@@ -1,24 +1,24 @@
 describe('Test for deleting steps', () => {
-    beforeEach(() => {
-        let url = Cypress.config().baseUrl;
-        cy.visit(url);
+  beforeEach(() => {
+    let url = Cypress.config().baseUrl;
+    cy.visit(url);
+  });
+  it('loads the YAML editor', () => {
+    cy.viewport(2000, 1000);
+    cy.get('[data-testid="toolbar-show-code-btn"]').click();
+    cy.get('.code-editor').click().type('{selectall} {backspace}')
+    cy.get('.pf-c-empty-state__secondary > .pf-c-button').click()
+    cy.fixture('delete.txt').then((user) => {
+      cy.get('.code-editor').type(user);
+      cy.wait(1000);
+      cy.get('[data-testid="react-flow-wrapper"]').contains('chuck-norris').click();
+      cy.wait(1000);
+      cy.get('.pf-c-drawer__panel-main > :nth-child(2) > .pf-c-button').click();
+      cy.get('.code-editor').contains('chuck-norris-s..').should('not.exist');
+      cy.get('[data-testid="react-flow-wrapper"]').contains('chunk-template..').click();
+      cy.get('.pf-c-drawer__panel-main > :nth-child(2) > .pf-c-button').click();
+      cy.get('[data-testid="react-flow-wrapper"]').contains('kafka-sink').click();
+      cy.get(':nth-child(2) > .pf-c-button').click();
     });
-    it('loads the YAML editor', () => {
-        cy.get('[data-testid="toolbar-show-code-btn"]').click(); 
-        cy.get('.code-editor').click();
-        cy.get('button').contains('Start from scratch').click();
-        cy.fixture('delete.txt')
-            .then((user) => {
-                cy.get('.code-editor').type(user);
-                cy.wait(1000)
-                cy.get('[data-testid="react-flow-wrapper"]').contains('chuck-norris').click();
-                cy.wait(1000)
-                cy.get(':nth-child(2) > .pf-c-button').click()
-                cy.get('.code-editor').contains('chuck-norris-s..').should('not.exist');
-                cy.get('[data-testid="react-flow-wrapper"]').contains('chunk-template..').click()
-                cy.get(':nth-child(2) > .pf-c-button').click()
-                cy.get('[data-testid="react-flow-wrapper"]').contains('kafka-sink').click()
-                cy.get(':nth-child(2) > .pf-c-button').click()
-            });
-    })
+  });
 });
--- cypress/integration/9-undo_redo_actions/undo_redo_actions.spec.js
+++ cypress/integration/9-undo_redo_actions/undo_redo_actions.spec.js
@@ -1,28 +1,31 @@
 describe('Test for undo/redo actions on code-editor', () => {
-    beforeEach(() => {
-        let url = Cypress.config().baseUrl;
-        cy.visit(url);
+  beforeEach(() => {
+    let url = Cypress.config().baseUrl;
+    cy.visit(url);
+  });
+
+  it('loads the YAML editor', () => {
+    cy.viewport(2000, 1000);
+    const dataTransfer = new DataTransfer();
+    cy.get('[data-testid="toolbar-show-code-btn"]').click()
+    cy.get('.code-editor').click().type('{selectall} {backspace}')
+    cy.get('.pf-c-empty-state__secondary > .pf-c-button').click()
+    cy.fixture('undo_redo.txt').then((user) => {
+      cy.get('.code-editor').type(user);
+      cy.wait(2000);
+      cy.get('.code-editor')
+        .contains('kafka-source')
+        .type(
+          '{end}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}timer-source',
+          { delay: 500 }
+        );
+      cy.get('[aria-label="Undo change"] > svg').click();
+      cy.get('[aria-label="Undo change"] > svg').click();
+      cy.get('[data-testid="react-flow-wrapper"]').contains('kafka-source');
+      cy.get('[aria-label="Redo change"] > svg').click();
+      cy.get('[aria-label="Redo change"] > svg').click();
+      cy.get('[data-testid="react-flow-wrapper"]').contains('timer-source');
+      cy.wait(2000);
     });
-  
-    it('loads the YAML editor', () => {
-        const dataTransfer = new DataTransfer();
-        cy.get('[data-testid="toolbar-show-code-btn"]').click();
-        cy.get('.pf-c-empty-state__secondary > .pf-c-button').click();
-        cy.fixture('undo_redo.txt')
-            .then((user) => {
-                cy.get('.code-editor').type(user);
-                cy.wait(2000)
-                cy.get('.code-editor')
-                cy.should('contain.text', 'kafka-source')
-                .type('{end}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}timer-source', { delay: 500 });
-                cy.get('[aria-label="Undo change"] > svg').click()
-                cy.get('[aria-label="Undo change"] > svg').click()
-                cy.get('[data-testid="react-flow-wrapper"]').contains('kafka-source')
-                cy.get('[aria-label="Redo change"] > svg').click()
-                cy.get('[aria-label="Redo change"] > svg').click()
-                cy.get('[data-testid="react-flow-wrapper"]').contains('timer-source')
-                cy.wait(2000)
-            })
-    })
- })
-  
+  });
+});
