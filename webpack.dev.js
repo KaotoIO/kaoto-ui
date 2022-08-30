69166fcbd30ff28fafc3ac5c4d05982c9628f6fe
Mohammadi Iram <mohammadiiram@gmail.com>
2022-08-30 14:38:42 +0530

    Migrating Cypress Version

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./webpack.config.js');
 const webpack = require('webpack');
 
 module.exports = merge(common('development'), {
