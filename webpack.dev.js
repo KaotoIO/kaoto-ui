c8121d59b2939c5f59cad54ededc7cfb6cc560f7
Mohammadi Iram <mohammadiiram@gmail.com>
2022-09-05 16:44:18 +0530

    Reverting back to webpack.common.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.config.js');
+const common = require('./webpack.common.js');
 const webpack = require('webpack');
 
 module.exports = merge(common('development'), {
