ceb35ff23fcc29e674190f7eda5dad7bba74dc34
Mohammadi Iram <mohammadiiram@gmail.com>
2022-09-05 16:44:18 +0530

    Reverting back to webpack.common.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 const { merge } = require('webpack-merge');
-const common = require('./webpack.config.js');
+const common = require('./webpack.common.js');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const webpack = require('webpack');
 
