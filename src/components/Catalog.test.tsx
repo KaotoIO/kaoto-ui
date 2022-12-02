093850ff5bfa86a206a733724035a9892ff153c3
Matej Melko <6814482+mmelko@users.noreply.github.com>
2022-12-02 14:24:43 +0100

    chore(tests): add tests for the Catalog component (#987)

--- src/components/Catalog.test.tsx
+++ src/components/Catalog.test.tsx
@@ -1,17 +1,137 @@
 import { AlertProvider } from '../layout';
 import { Catalog } from './Catalog';
+import { jest } from '@jest/globals';
+import { fetchCatalogSteps } from '@kaoto/api';
 import { screen } from '@testing-library/dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
+jest.mock('@kaoto/api');
+const mockedValue = [
+  {
+    name: 'end-step',
+    type: 'END',
+    id: 'aggregate',
+    kind: 'EIP',
+    icon: 'icon',
+    title: 'Aggregate',
+    description:
+      'The Aggregator from the EIP patterns allows you to combine a number of messages together into a single message.',
+    group: 'Knative',
+    minBranches: 0,
+    maxBranches: 0,
+    UUID: null,
+  },
+  {
+    name: 'claim-check',
+    type: 'MIDDLE',
+    id: 'claim-check',
+    kind: 'EIP',
+    icon: 'icon',
+    title: 'Claim Check',
+    description:
+      'The Claim Check from the EIP patterns allows you to replace message content with a claim check (a unique key), which can be used to retrieve the message content at a later time.',
+    group: 'Knative',
+    minBranches: 0,
+    maxBranches: 0,
+    UUID: null,
+  },
+  {
+    name: 'start-step',
+    type: 'START',
+    id: 'start',
+    kind: 'Start-step-kind',
+    icon: 'icon',
+    title: 'Start-step',
+    description: 'Kubernetes resource whose URL can be retrieved by reading its status.address.url',
+    group: 'start-step-group',
+    minBranches: 0,
+    maxBranches: 0,
+    UUID: null,
+  },
+  {
+    name: 'unmarshal',
+    type: 'MIDDLE',
+    id: 'unmarshal',
+    kind: 'EIP',
+    icon: 'icon',
+    title: 'Unmarshal',
+    description:
+      'Transforms data in some binary or textual format (such as received over the network) into a Java object; or some other representation according to the data format being used.',
+    group: 'Knative',
+    minBranches: 0,
+    maxBranches: 0,
+    UUID: null,
+  },
+];
+
+let method: jest.Mock;
 describe('Catalog.tsx', () => {
-  test('component renders correctly', () => {
+  beforeAll(() => {
+    method = (fetchCatalogSteps as jest.Mock).mockResolvedValue(mockedValue);
+  });
+
+  test('component renders correctly', async () => {
+    render(
+      <AlertProvider>
+        <Catalog handleClose={jest.fn()} />
+      </AlertProvider>
+    );
+    //test if mocked method have been called
+    expect(method).toHaveBeenCalled();
+    //find the start-step
+    let element = await screen.findByText('start-step');
+    expect(element).toBeInTheDocument();
+
+    element = await screen.findByText('actions');
+    expect(element).toBeInTheDocument();
+    element.click();
+
+    element = await screen.findByText('claim-check');
+    expect(element).toBeInTheDocument();
+
+    element = await screen.findByText('end');
+    expect(element).toBeInTheDocument();
+    element.click();
+
+    element = await screen.findByText('end-step');
+    expect(element).toBeInTheDocument();
+  });
+
+  test('Search input works correctly', async () => {
+    const user = userEvent.setup();
     render(
       <AlertProvider>
         <Catalog handleClose={jest.fn()} />
       </AlertProvider>
     );
 
-    const element = screen.getByTestId('stepCatalog');
+    expect(method).toHaveBeenCalled();
+
+    let element = await screen.findByText('actions');
     expect(element).toBeInTheDocument();
+    await user.click(element);
+
+    await user.click(screen.getByPlaceholderText('search for a step...'));
+    await user.keyboard('unmar');
+
+    expect(screen.queryByText('claim-check')).not.toBeInTheDocument();
+    expect(screen.getByText('unmarshal')).toBeInTheDocument();
+  });
+
+  test('Alert is fired when there is an error with fetching', async () => {
+    method = (fetchCatalogSteps as jest.Mock).mockImplementation(() => Promise.reject('fail'));
+    render(
+      <AlertProvider>
+        <Catalog handleClose={jest.fn()} />
+      </AlertProvider>
+    );
+
+    expect(method).toHaveBeenCalled();
+    expect(
+      await screen.findByText(
+        'There was a problem fetching the catalog steps. Please try again later.'
+      )
+    ).toBeInTheDocument();
   });
 });
