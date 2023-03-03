a646055c78fa9b2784875afe75e36f2684546449
tplevko <tplevko@redhat.com>
2023-03-03 16:34:12 +0100

    Add basic e2e editor tests for step CRUD operations

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,26 +1,26 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import 'cypress-file-upload';
+
+Cypress.Commands.add('editorAddText', (line, text) => {
+  var arr = text.split('\n');
+  Cypress._.times(arr.length, (i) => {
+    cy.get('.code-editor')
+      .click()
+      .type('{pageUp}{pageUp}' + '{downArrow}'.repeat(line + i) + '{enter}{upArrow}' + arr[i], {
+        delay: 1,
+      });
+  });
+});
+
+Cypress.Commands.add('editorDeleteLine', (line, repeatCount) => {
+  repeatCount = repeatCount ?? 1;
+  cy.get('.code-editor')
+    .click()
+    .type(
+      '{pageUp}' +
+        '{downArrow}'.repeat(line) +
+        '{shift}' +
+        '{downArrow}'.repeat(repeatCount) +
+        '{backspace}',
+      { delay: 1 }
+    );
+});
