6ce10c23ba568f58c29e19119f24357e3708bc72
Ricardo M <lordrip@gmail.com>
2023-03-09 15:57:51 +0100

    chore: Extract a new AddStepButton component out of the VisualizationStep component
    Extracting this component section allows for a easier testing and will be (hopefully)
    better to reason about.
    
    Relates to https://github.com/KaotoIO/kaoto-ui/issues/1381

---
+++ src/components/AddStepButton.tsx
@@ -0,0 +1,75 @@
+import { ValidationService } from '@kaoto/services';
+import { useSettingsStore } from '@kaoto/store';
+import { IStepProps } from '@kaoto/types';
+import { Popover, Tooltip } from '@patternfly/react-core';
+import { PlusIcon } from '@patternfly/react-icons';
+import { FunctionComponent } from 'react';
+import { BranchBuilder } from './BranchBuilder';
+import { MiniCatalog } from './MiniCatalog';
+
+interface IAddStepButton {
+  handleAddBranch: () => void;
+  handleSelectStep: (selectedStep: IStepProps) => void;
+  step: IStepProps;
+  showBranchesTab: boolean;
+  showStepsTab: boolean;
+  supportsBranching: boolean;
+}
+
+export const AddStepButton: FunctionComponent<IAddStepButton> = ({
+  handleAddBranch,
+  handleSelectStep,
+  step,
+  showBranchesTab,
+  showStepsTab,
+  supportsBranching,
+}) => {
+  const currentDSL = useSettingsStore((state) => state.settings.dsl.name);
+
+  return (
+    <Popover
+      id="popover-append-step"
+      appendTo={() => document.body}
+      aria-label="Add a step or branch"
+      bodyContent={
+        <MiniCatalog
+          children={<BranchBuilder handleAddBranch={handleAddBranch} />}
+          disableBranchesTab={!showBranchesTab}
+          disableBranchesTabMsg={ValidationService.getBranchTabTooltipMsg(
+            supportsBranching,
+            step.maxBranches,
+            step.branches?.length
+          )}
+          disableStepsTab={!showStepsTab}
+          disableStepsTabMsg={"You can't add a step between a step and a branch."}
+          handleSelectStep={handleSelectStep}
+          queryParams={{
+            dsl: currentDSL,
+            type: ValidationService.appendableStepTypes(step.type),
+          }}
+          step={step}
+        />
+      }
+      className={'miniCatalog__popover'}
+      data-testid={'miniCatalog__popover'}
+      enableFlip={true}
+      flipBehavior={['top-start', 'left-start']}
+      hasAutoWidth
+      hideOnOutsideClick={true}
+      position={'right-start'}
+      showClose={false}
+    >
+      <Tooltip
+        content={ValidationService.getPlusButtonTooltipMsg(showBranchesTab, showStepsTab)}
+      >
+        <button
+          className="stepNode__Add plusButton nodrag"
+          data-testid={'stepNode__appendStep-btn'}
+          aria-label="Add a step or a branch"
+        >
+          <PlusIcon />
+        </button>
+      </Tooltip>
+    </Popover>
+  );
+}
