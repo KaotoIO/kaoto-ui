f80c8b97714f1600070ae1fd91bf9b2cd2f16375
Ricardo M <lordrip@gmail.com>
2023-03-10 16:41:23 +0100

    chore: Add tests for AppendStepButton component
    Relates https://github.com/KaotoIO/kaoto-ui/issues/1381

--- setupTests.ts
+++ setupTests.ts
@@ -1,5 +1,15 @@
 import '@testing-library/jest-dom';
 import { TextEncoder } from 'util';
 import 'whatwg-fetch';
+import { stepsCatalog } from './src/__mocks__/steps';
 
 global.TextEncoder = TextEncoder;
+
+jest.mock('@kaoto/api', () => {
+  const actual = jest.requireActual('@kaoto/api');
+
+  return {
+    ...actual,
+    fetchCatalogSteps: jest.fn().mockResolvedValue(stepsCatalog),
+  };
+});
