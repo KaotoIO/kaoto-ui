0b48797b3a7a9dea5165f00e80680a3f9ff71a26
Tomohisa Igarashi <tm.igarashi@gmail.com>
2023-03-14 19:06:32 -0400

    fix: Hide parameters that are not supported in the default config panel
    Fixes: #1444

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import './App.css';
+import { useTemporalIntegrationJsonStore } from './store';
+import { bindUndoRedo } from './utils';
 import { AlertProvider, AppLayout, MASLoading } from '@kaoto/layout';
 import { AppRoutes } from '@kaoto/routes';
 import '@patternfly/patternfly/patternfly-theme-dark.css';
 import '@patternfly/patternfly/patternfly.css';
 import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
+import '@patternfly/patternfly/utilities/BackgroundColor/BackgroundColor.css';
 import '@patternfly/patternfly/utilities/Display/display.css';
 import '@patternfly/patternfly/utilities/Flex/flex.css';
 import '@patternfly/patternfly/utilities/Sizing/sizing.css';
 import '@patternfly/patternfly/utilities/Spacing/spacing.css';
-import 'reactflow/dist/style.css';
+import '@patternfly/patternfly/utilities/Text/text.css';
 import { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { bindUndoRedo } from './utils';
-import { useTemporalIntegrationJsonStore } from './store';
+import 'reactflow/dist/style.css';
 
 const { undo, redo } = useTemporalIntegrationJsonStore.getState();
 
--- src/components/CustomJsonSchemaBridge.tsx
+++ src/components/CustomJsonSchemaBridge.tsx
@@ -1,32 +1,12 @@
-import { Popover } from '@patternfly/react-core';
-import { HelpIcon } from '@patternfly/react-icons';
+import DisabledField from './DisabledField';
+import { FieldLabelIcon } from './FieldLabelIcon';
 import JSONSchemaBridge from 'uniforms-bridge-json-schema';
-
-/**
- * Returns a label tooltip element for the form or undefined if the field has no description
- * @param description 
- * @returns 
- */
-const getLabelIcon = (description: string) =>
-  typeof (description) !== 'undefined' ?
-    (
-      <Popover bodyContent={description}>
-        <button
-          type="button"
-          aria-label="More info for field"
-          aria-describedby="form-group-label-info"
-          className="pf-c-form__group-label-help"
-        >
-          <HelpIcon /></button>
-      </Popover>
-    ) : undefined;
 
 /**
  * CustomJsonSchemaBridge generates the appropriate attributes for uniforms-patternfly
  * based on the incoming model data
  */
 export class CustomJsonSchemaBridge extends JSONSchemaBridge {
-
   constructor(schema: any, validator: any) {
     super(schema, validator);
   }
@@ -35,9 +15,16 @@ export class CustomJsonSchemaBridge extends JSONSchemaBridge {
     const field = super.getField(name);
     const { description, ...props } = field;
 
-    return {
-      labelIcon: getLabelIcon(description),
-      ...props
+    const isDisabled = field.type === 'object';
+    const revisedField: any = {
+      labelIcon: description ? FieldLabelIcon({ description, disabled: isDisabled }) : undefined,
+      ...props,
     };
+    if (isDisabled) {
+      revisedField.uniforms = {
+        component: DisabledField,
+      };
+    }
+    return revisedField;
   }
 }
