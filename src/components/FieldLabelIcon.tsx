f1acf9473e76e99bf5bb2fd52167b901ff0b6d4d
kahboom <rachelyordan@gmail.com>
2023-03-23 18:32:16 +0000

    fix(stepDetail): show default value in popover

--- src/components/FieldLabelIcon.tsx
+++ src/components/FieldLabelIcon.tsx
@@ -1,21 +1,35 @@
-import { Button, Popover } from '@patternfly/react-core';
+import { Button, Popover, Text, TextVariants } from '@patternfly/react-core';
 import { HelpIcon } from '@patternfly/react-icons';
 
 type FieldLabelIconProps = {
+  defaultValue?: any;
   description?: string;
   disabled: boolean;
 };
 
 /**
  * Returns a label tooltip element for the form or undefined if the field has no description
- * @param description
  * @returns
+ * @param props
  */
 export const FieldLabelIcon = (props: FieldLabelIconProps) => {
   const headerContent = props.disabled
     ? 'Please use the source code editor to configure this property.'
     : '';
-  const bodyContent = props.description ? props.description : '';
+  const bodyContent = () => {
+    return props.description ? (
+      <>
+        {props.description}
+        <>
+          <br />
+          <br />
+          <Text component={TextVariants.small}>Default: {props.defaultValue ?? 'null'}</Text>
+        </>
+      </>
+    ) : (
+      ''
+    );
+  };
   return (
     <Popover headerContent={headerContent} bodyContent={bodyContent}>
       <Button
