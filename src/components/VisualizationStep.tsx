9f3fe76f2abe5ade6a15f7875b7386dcf92a2e9c
Mikhail Abramov <46084942+unsortedhashsets@users.noreply.github.com>
2023-04-17 14:17:18 +0200

    chore(viz): Adjust Node label clickable area (#1664)

--- src/components/VisualizationStep.tsx
+++ src/components/VisualizationStep.tsx
@@ -175,8 +175,8 @@ const VisualizationStep = ({ data }: NodeProps<IVizStepNodeData>) => {
             <img src={data.step.icon} alt={data.label} />
           </div>
           {/* STEP LABEL */}
-          <div className={'stepNode__Label stepNode__clickable'}>
-            <span>{data.label}</span>
+          <div className={'stepNode__Label'}>
+            <span className={'stepNode__clickable'}>{data.label}</span>
           </div>
           {/* RIGHT-SIDE HANDLE FOR EDGE TO CONNECT WITH */}
           {!StepsService.isEndStep(data.step) && (
@@ -264,7 +264,9 @@ const VisualizationStep = ({ data }: NodeProps<IVizStepNodeData>) => {
               id="b"
               isConnectable={false}
             />
-            <div className={'stepNode__Label stepNode__clickable'}>{data.label}</div>
+            <div className={'stepNode__Label'}>
+              <span className={'stepNode__clickable'}>{data.label}</span>
+            </div>
           </div>
         </Popover>
       )}
