11d20c9a9d128afd1a433c7d72cb070287e7c793
Mikhail Abramov <46084942+unsortedhashsets@users.noreply.github.com>
2023-04-27 18:52:36 +0200

    chore(e2e): Update appearance tests (#1704)

--- cypress/e2e/01-settings/appearance.cy.js
+++ cypress/e2e/01-settings/appearance.cy.js
@@ -16,28 +16,54 @@ describe('Settings: Appearance', () => {
     cy.get('[data-testid="appearance-modal"').should('be.visible');
   });
 
-  it('enables dark mode in Kaoto', () => {
-    cy.switchAppearanceTheme()
+  it('Enables dark mode in Kaoto', () => {
+    cy.switchAppearanceTheme().should(() => {
+      expect(localStorage.getItem('KAOTO_UI_THEME_IS_LIGHT')).to.eq('false');
+    });
 
     // Check that theme is dark
     cy.get('.pf-theme-dark').should('exist');
     cy.get('html').should('have.class', 'pf-theme-dark').and('have.css', 'color-scheme', 'dark');
 
-    cy.switchAppearanceTheme()
+    // Reload Page and check that theme is still dark
+    cy.openHomePage().should(() => {
+      expect(localStorage.getItem('KAOTO_UI_THEME_IS_LIGHT')).to.eq('false');
+    });
+    cy.get('.pf-theme-dark').should('exist');
+    cy.get('html').should('have.class', 'pf-theme-dark').and('have.css', 'color-scheme', 'dark');
+
+    cy.openAppearanceModal();
+    cy.switchAppearanceTheme().should(() => {
+      expect(localStorage.getItem('KAOTO_UI_THEME_IS_LIGHT')).to.eq('true');
+    });
 
     // Check that theme is not dark
     cy.get('.pf-theme-dark').should('not.exist');
-    cy.get('html')
-      .should('not.have.class', 'pf-theme-dark')
-      .and('not.have.css', 'color-scheme', 'dark');
+    cy.get('html').should('not.have.class', 'pf-theme-dark').and('not.have.css', 'color-scheme', 'dark');
+
+    // Reload Page and check that theme is still not dark
+    cy.openHomePage().should(() => {
+      expect(localStorage.getItem('KAOTO_UI_THEME_IS_LIGHT')).to.eq('true');
+    });;
+    cy.get('.pf-theme-dark').should('not.exist');
+    cy.get('html').should('not.have.class', 'pf-theme-dark').and('not.have.css', 'color-scheme', 'dark');
   });
 
-  it('enables light mode in code editor', () => {
-    cy.switchAppearanceTheme('editor')
+  it('Enables light mode in code editor', () => {
+    cy.switchAppearanceTheme('editor').should(() => {
+      expect(localStorage.getItem('KAOTO_EDITOR_THEME_IS_LIGHT')).to.eq('true');
+    });
     cy.closeAppearanceModal();
-    cy.openCodeEditor();
 
     // Check that theme is not dark
+    cy.openCodeEditor();
+    cy.get('.monaco-scrollable-element').should('not.have.class', 'vs-dark');
+
+    // Reload Page and check that theme is still not dark
+    cy.openHomePage().should(() => {
+      expect(localStorage.getItem('KAOTO_EDITOR_THEME_IS_LIGHT')).to.eq('true');
+    });
+    cy.openCodeEditor();
     cy.get('.monaco-scrollable-element').should('not.have.class', 'vs-dark');
 
     cy.openAppearanceModal();
@@ -45,6 +71,13 @@ describe('Settings: Appearance', () => {
     cy.closeAppearanceModal();
 
     // Check that theme is dark
+    cy.get('.monaco-scrollable-element').should('have.class', 'vs-dark');
+
+    // Reload Page and check that theme is still dark
+    cy.openHomePage().should(() => {
+      expect(localStorage.getItem('KAOTO_EDITOR_THEME_IS_LIGHT')).to.eq('false');
+    });
+    cy.openCodeEditor();
     cy.get('.monaco-scrollable-element').should('have.class', 'vs-dark');
   });
 });
