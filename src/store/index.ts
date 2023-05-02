afdfe4e25dbbc4bf1b2f039cd4463fb0c9338f18
Ricardo M <lordrip@gmail.com>
2023-05-02 23:27:01 +0200

    chore: Create new FlowsStore that supports multiple routes

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,26 @@
+import { mountStoreDevtool } from 'simple-zustand-devtools';
+import { useDeploymentStore } from './deploymentStore';
+import { flowsStore } from './flowsStore';
+import { useIntegrationJsonStore } from './integrationJsonStore';
+import { useIntegrationSourceStore } from './integrationSourceStore';
+import { useNestedStepsStore } from './nestedStepsStore';
+import { useSettingsStore } from './settingsStore';
+import { useStepCatalogStore } from './stepCatalogStore';
+import { useVisualizationStore } from './visualizationStore';
+
+if (process.env.NODE_ENV === 'development') {
+  mountStoreDevtool('deploymentStore', useDeploymentStore);
+  mountStoreDevtool('flowsStore', flowsStore);
+  mountStoreDevtool('integrationJsonStore', useIntegrationJsonStore);
+  mountStoreDevtool('integrationSourceStore', useIntegrationSourceStore);
+  mountStoreDevtool('nestedStepsStore', useNestedStepsStore);
+  mountStoreDevtool('settingsStore', useSettingsStore);
+  mountStoreDevtool('stepCatalogStore', useStepCatalogStore);
+  mountStoreDevtool('visualizationStore', useVisualizationStore);
+}
+
 export * from './deploymentStore';
+export * from './flowsStore';
 export * from './integrationJsonStore';
 export * from './integrationSourceStore';
 export * from './nestedStepsStore';
