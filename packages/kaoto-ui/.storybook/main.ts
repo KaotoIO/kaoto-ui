b53da29a38b91c044be3ef3e1c120dbf9084b4ec
Ricardo M <lordrip@gmail.com>
2023-07-20 07:52:19 +0200

    chore: fix deepscan issues

--- packages/kaoto-ui/.storybook/main.ts
+++ packages/kaoto-ui/.storybook/main.ts
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const prod = require('../webpack.prod.js');
 const path = require('path');
 const webpack = require('webpack');
 const { version } = require('../package.json');
